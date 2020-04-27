a0730cc5229274ec6abe7f8c638c3d9eeaf3b98a
Melody Soriano <mel.soriano6123@gmail.com>
2020-04-27 22:23:11 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    current_player = current_player(board)
+    player = current_player(board)
     move(board, index)
     display_board(board)
   else
